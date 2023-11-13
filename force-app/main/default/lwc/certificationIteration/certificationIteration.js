7a324b7cd0caecc941987d31c1e5d4a4c6742481
Raza Hussain <RazaHussain090@gmail.com>
2023-11-13 18:21:53 +0500

    certificationIteration

---
+++ force-app/main/default/lwc/certificationIteration/certificationIteration.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+
+export default class CertificationIteration extends LightningElement {
+    certificates = [
+        {
+            Name: 'Platform Developer 1',
+            Picture: 'https://razahussain090-dev-ed.my.site.com/portfolio/resource/13237000/Platform_Developer_1',
+            Date: '12/2/2022'
+        },
+        {
+            Name: 'Platform Appbuilder',
+            Picture: 'https://razahussain090-dev-ed.my.site.com/portfolio/resource/13237000/Platform_Appbuilder',
+            Date: '12/2/2022'
+        }
+    ];
+}
